e2e1b4dac55ee2e7fec1c390240a5de376746280
sergioandux <sergioandux@gmail.com>
2021-11-25 15:42:25 -0300

    programa dias completo

---
+++ index.js
@@ -0,0 +1,5 @@
+var diasinicio = parseInt(prompt("ingrese una cantidad de dias"));
+var anos = parseInt(diasinicio/365);
+var semanas = parseInt((diasinicio%365)/7);
+var diasfinal = parseInt((diasinicio%365)%7);
+document.write(diasinicio+" dias equivale a: "+anos+" años, "+semanas+" semanas, "+diasfinal+" dias.");
